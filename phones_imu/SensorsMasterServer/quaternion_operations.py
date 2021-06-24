3c511fb03695dd9d5558354f9b96261000d077c6
KanapkaBezMasla <58138205+KanapkaBezMasla@users.noreply.github.com>
2021-06-24 14:41:09 +0200

    Update quaternion_operations.py

--- phones_imu/SensorsMasterServer/quaternion_operations.py
+++ phones_imu/SensorsMasterServer/quaternion_operations.py
@@ -1,4 +1,32 @@
 from scipy.spatial.transform import Rotation as R
 
-def rotate_180_x(quaternion):
-    rotated =
+
+def rotate180x(quat):
+    r2 = R.from_matrix([[1, 0, 0],
+                        [0, -1, 0],
+                        [0, 0, -1]])
+    r3 = rotate(quat, r2)
+    return r3
+
+
+def rotate180y(quat):
+    r2 = R.from_matrix([[-1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, -1]])
+    r3 = rotate(quat, r2)
+    return r3
+
+
+def rotate180x(quat):
+    r2 = R.from_matrix([[-1, 0, 0],
+                        [0, -1, 0],
+                        [0, 0, 1]])
+    r3 = rotate(quat, r2)
+    return r3
+
+
+def rotate(quat, r2):
+    r1 = R.from_quat(quat)
+    r3 = r2 * r1
+    return r3
+
