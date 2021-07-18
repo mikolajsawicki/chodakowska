52d1a0829e69186df9ada2896958bc675d65ff96
Mikołaj Sawicki <37419432+mikolajsawicki@users.noreply.github.com>
2021-07-18 19:10:17 +0200

    Update blender_interface.py

--- phones_imu/SensorsMasterServer/blender_interface.py
+++ phones_imu/SensorsMasterServer/blender_interface.py
@@ -1,26 +1,3 @@
-# Chordata Open Pose Protocol (COPP) server
-# -- Pose data server for the Chordata Open Source motion capture system
-#
-# http://chordata.cc
-# contact@chordata.cc
-#
-# Copyright 2019 Bruno Laurencich
-#
-# This file is part of Chordata Open Pose Protocol (COPP) server.
-#
-# COPP server is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# COPP server is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with COPP server.  If not, see <https://www.gnu.org/licenses/>.
-
 import socket
 import time
 from copp_server.osc4py3_buildparse import oscbuildparse as oscparse
